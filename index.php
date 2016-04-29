cf39ea05d335c19d42b4fc6660d2e487f448f240
Ad5001 <ad5001yt@gmail.com>
2016-04-29 09:33:39 +0200

    Creating the get of resource (/!\ Not disigned yet /!\)

--- index.php
+++ index.php
@@ -1,8 +1,42 @@
-<html><head><title>BoxManager</title></head>
+<html><head><title>BoxManager</title>
+<link rel="icon" href="favicon.png" />
+<link rel="stylesheet" href="/css/skel.css" />
+<link rel="stylesheet" href="/css/style.css" />
+<link rel="stylesheet" href="/css/style-xlarge.css" />
+</head>
 <body>
 <?php
+$resources = [];
 $dir = 'resources';
-$files = array_diff(scandir($directory), array('..', '.'));
+$files = array_diff(scandir($dir), array('..', '.')); // getting all resources
+foreach($files as $file) {
+	$contents = file_get_contents($file);
+	$lines = explode("\n", $contents);
+	foreach($lines as $line) {
+		if(preg_match("/:/i")) {
+			$linediff = explode(":", $line); // Getting title, version, ect
+			switch(strtolower($linediff[0])) {
+				case "title": // if this is the title
+				unset($linediff[0]);
+				$title = implode(":", $linediff);
+				unset($line);
+				break;
+				case "version": // if this is the vzrsion
+				unset($linediff[0]);
+				$version = implode(":", $linediff);
+				unset($line);
+				break;
+				case "download": // if this is the download link
+				unset($linediff[0]);
+				$dllink = implode(":", $linediff);
+				unset($line);
+				break;
+			}
+		}
+	}
+	$contents = implode("\n", $lines);
+	array_push($reources, $contents);
+}
 ?>
 </body>
 </html>

8463c5e77993f7abf5be844f9b660aabd5b1603b
UltimateMcraft <UltimateMcraft@users.noreply.github.com>
2016-04-29 13:57:02 +0300

    Create membersite_config.php

---
+++ signup/membersite_config.php
@@ -0,0 +1,25 @@
+<?PHP
+require_once("fg_membersite.php");
+
+$fgmembersite = new FGMembersite();
+
+//Site name
+$fgmembersite->SetWebsiteName('boxofdevs.x10host.com');
+
+//Provide the admin email (notifications)
+$fgmembersite->SetAdminEmail('me@example.com');
+
+//Provide your database login details here:
+//hostname, user name, password, database name and table name
+//note that the script will create the table (for example, resource in this case)
+//by itself on submitting register.php for the first time
+$fgmembersite->InitDB(/*hostname*/'localhost',
+                      /*username*/'user',
+                      /*password*/'pass',
+                      /*database name*/'dbname',
+                      /*table name*/'resource');
+
+
+$fgmembersite->SetRandomKey('qSRcVS6DrTzrPvr');
+
+?>

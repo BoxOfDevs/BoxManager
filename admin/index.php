0d4422ab768f540d848e9a807dfecb264f1a6ca6
UltimateMcraft <UltimateMcraft@users.noreply.github.com>
2016-04-30 09:45:20 +0100

    Create index.php

---
+++ admin/index.php
@@ -0,0 +1,20 @@
+<?php
+session_start(); //Start the session
+define(ADMIN,$_SESSION['name']); //Get the user name from the previously registered super global variable
+if(!session_is_registered("admin")){ //If session not registered
+header("location:login.php"); // Redirect to login.php page
+}
+else //Continue to current page
+header( 'Content-Type: text/html; charset=utf-8' );
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+    <title>Admin CP</title>
+</head>
+<body>
+    <h1>Welcome To Admin Page <?php echo ADMIN /*Echo the username */ ?></h1>
+    <p><a href="logout.php">Logout</a></p> <!-- A link for the logout page -->
+    <p>ADMIN PAGE CONTENTS HERE</p>
+</body>
+</html>

ee96e6f6a315b25931cd21c75b62f6e0944ef975
UltimateMcraft <UltimateMcraft@users.noreply.github.com>
2016-05-05 17:07:10 +0300

    only allow logged in users to view page

--- login-home.php
+++ login-home.php
@@ -1,2 +1,9 @@
-Notice: HOME PAGE FOR LOGGED IN USERS GO HERE!
+<?PHP
+require_once("./login/membersite_config.php"); // Check config
 
+if(!$fgmembersite->CheckLogin()) // Check login
+{
+    $fgmembersite->RedirectToURL("login.php"); // If not logged in, redirect.
+    exit;
+}
+?>

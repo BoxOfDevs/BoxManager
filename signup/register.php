cb9159cb642f564a1f164671d882f456af292d89
UltimateMcraft <UltimateMcraft@users.noreply.github.com>
2016-05-09 18:06:50 +0300

    Update register.php

--- signup/register.php
+++ signup/register.php
@@ -90,5 +90,5 @@ Uses the excellent form validation script from JavaScript-coder.com-->
 <!--
 Form Code End
 -->
-
+<p><a href="http://boxofdevs.ml>Powered by BoxManager - Copyright © BoxOfDevs Team 2016.</a></p>
 </body></html>

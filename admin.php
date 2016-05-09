e388fc1173cd3d805e4a2460ef135051153ce9bd
TheDragonRing <tom.vines@rocketmail.com>
2016-05-10 09:52:09 +1000

    Update admin.php

--- admin.php
+++ admin.php
@@ -27,5 +27,5 @@ echo "<h1>Please enter your Login Information</h1>";
 echo $login_form;
 ?>
 </body>
-<p><a href="http://boxofdevs.ml>© 2016 BoxManager - BoxOfDevs team.</a></p>
+<p><a href="http://boxofdevs.ml">Powered by BoxManager - Copyright © 2016 BoxOfDevs Team</a></p>
 </html>

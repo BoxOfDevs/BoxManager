13bdb1a656891b152a2c2bc477d9c4379bfbef26
UltimateMcraft <UltimateMcraft@users.noreply.github.com>
2016-05-21 12:40:32 +0300

    Add comments

--- reader.php
+++ reader.php
@@ -1,4 +1,16 @@
 <?php
+require('comments/Persistence.php');
+$comment_post_ID = 1;
+$db = new Persistence();
+$comments = $db->get_comments($comment_post_ID);
+$has_comments = (count($comments) > 0);
+ /* ____            __  __                                   
+ * | __ )  _____  _|  \/  | __ _ _ __   __ _  __ _  ___ _ __ 
+ * |  _ \ / _ \ \/ / |\/| |/ _` | '_ \ / _` |/ _` |/ _ \ '__|
+ * | |_) | (_) >  <| |  | | (_| | | | | (_| | (_| |  __/ |   
+ * |____/ \___/_/\_\_|  |_|\__,_|_| |_|\__,_|\__, |\___|_|   
+ *                                          |___/     
+ */
 error_reporting(-1);
 if (isset($_GET["thread"])) {
 	$id = htmlspecialchars($_GET["thread"]);
@@ -55,6 +67,61 @@ if(!isset($id)) {
 				</div>
 			</header>
 </section>
+<section id="comments" class="body">
+	  
+	  <header>
+			<h2>Comments</h2>
+		</header>
+
+    <ol id="posts-list" class="hfeed<?php echo($has_comments?' has-comments':''); ?>">
+      <li class="no-comments">Be the first to add a comment.</li>
+      <?php
+        foreach ($comments as &$comment) {
+          ?>
+          <li><article id="comment_<?php echo($comment['id']); ?>" class="hentry">	
+    				<footer class="post-info">
+    					<abbr class="published" title="<?php echo($comment['date']); ?>">
+    						<?php echo( date('d F Y', strtotime($comment['date']) ) ); ?>
+    					</abbr>
+
+    					<address class="vcard author">
+    						By <a class="url fn" href="#"><?php echo($comment['comment_author']); ?></a>
+    					</address>
+    				</footer>
+
+    				<div class="entry-content">
+    					<p><?php echo($comment['comment']); ?></p>
+    				</div>
+    			</article></li>
+          <?php
+        }
+      ?>
+		</ol>
+		
+		<div id="respond">
+
+      <h3>Leave a Comment</h3>
+
+      <form action="commments/post_comment.php" method="post" id="commentform">
+
+        <label for="comment_author" class="required">Your name</label>
+        <input type="text" name="comment_author" id="comment_author" value="" tabindex="1" required="required">
+        
+        <label for="email" class="required">Your email</label>
+        <input type="email" name="email" id="email" value="" tabindex="2" required="required">
+
+        <label for="comment" class="required">Your message</label>
+        <textarea name="comment" id="comment" rows="10" tabindex="4"  required="required"></textarea>
+
+        <input type="hidden" name="comment_post_ID" value="<?php echo($comment_post_ID); ?>" id="comment_post_ID" />
+        <input name="submit" type="submit" value="Submit comment" />
+        
+      </form>
+      
+    </div>
+			
+	</section>
+	
 </body>
 <p><a href="http://boxofdevs.ml">© 2016 BoxManager - BoxOfDevs team.</a></p>
 </html>
